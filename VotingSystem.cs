b2744acfee09a2c193c281fa94509bdf45670d54
hissbot <hisstwitchbot@gmail.com>
2015-07-16 21:19:22 -0500

    a

---
+++ VotingSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Meebey.SmartIrc4net;
+
+namespace TwitchChatBot
+{
+    class VotingSystem :ChatModule
+    {
+        public override void ProcessMessage(IrcEventArgs e)
+        {
+            ChatBot.say("Still working on inplementing this!");
+        }
+
+        public VotingSystem()
+        {
+            initCommands = new string[] { "!vote", "!voteNo", "!voteYes" };
+        }
+    }
+}
